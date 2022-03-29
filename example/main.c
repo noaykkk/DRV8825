c1c0bb7d9a977a7ba9b457177b8fd06484405f0e
noaykkk <idnoay@gmail.com>
2022-03-28 20:53:55 -0400

    DC motor extend
    add DC motor control

--- example/main.c
+++ example/main.c
@@ -1,24 +1,49 @@
 #include <stdbool.h>
 #include <stdint.h>
-#include "inc/hw_types.h"
-#include "inc/hw_memmap.h"
-#include "driverlib/sysctl.h"
+#include "driverlib/adc.h"
 #include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
 int main(void)
 {
     SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    SysCtlDelay(10);
+
+    HWREG(GPIO_PORTF_BASE+GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTF_BASE+GPIO_O_CR) |= 0x01;
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0);
+    uint32_t PinData;
+
     while(1)
     {
-        GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3, 0x04);
+        PinData = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
+        if( (PinData == GPIO_PIN_0))
+        {
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5, 0x04);
 
-        SysCtlDelay(500);
+            SysCtlDelay(20000000);
 
-        GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3, 0x0C);
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5, 0x0C);
 
-        SysCtlDelay(500);
+            SysCtlDelay(20000000);
+        }
+        else
+        {
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5, 0x14);
 
+            SysCtlDelay(20000000);
+
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5, 0x1C);
+
+            SysCtlDelay(20000000);
+        }
 
     }
 }
