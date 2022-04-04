78c402fb69c7faa5d640ad992a9bd7a4d13083a3
noaykkk <idnoay@gmail.com>
2022-04-04 18:02:37 -0400

    Add PWM module

--- example/main.c
+++ example/main.c
@@ -5,18 +5,52 @@
 #include "driverlib/pin_map.h"
 #include "driverlib/rom_map.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/pwm.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 #include "inc/hw_gpio.h"
 int main(void)
 {
+    // Set the clock
     SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+    // Configure PWM Clock
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
+
+    // Enable the peripherals
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
     SysCtlDelay(10);
 
+    // Remove the lock
     HWREG(GPIO_PORTF_BASE+GPIO_O_LOCK) = GPIO_LOCK_KEY;
     HWREG(GPIO_PORTF_BASE+GPIO_O_CR) |= 0x01;
+
+    // Configure PF1 as PWM output
+    GPIOPinConfigure(GPIO_PF1_M1PWM5);
+    GPIOPinConfigure(GPIO_PF2_M1PWM6);
+    GPIOPinConfigure(GPIO_PF3_M1PWM7);
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+
+    // Configure PWM options
+    PWMGenConfigure(PWM1_BASE, PWM_GEN_2, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
+    PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
+
+    //Set the Period (expressed in clock ticks)
+    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_2, 320);
+    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, 320);
+
+    //Set PWM duty-50% (Period /2)
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5,100);
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,100);
+    PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,100);
+
+    // Enable the PWM generator
+    PWMGenEnable(PWM1_BASE, PWM_GEN_2);
+    PWMGenEnable(PWM1_BASE, PWM_GEN_3);
+
+    // Set outputs pins
     GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5);
     GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0);
     uint32_t PinData;
@@ -26,6 +60,9 @@ int main(void)
         PinData = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
         if( (PinData == GPIO_PIN_0))
         {
+            // Turn off the Output pins
+            PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT | PWM_OUT_6_BIT | PWM_OUT_7_BIT, false);
+
             GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5, 0x04);
 
             SysCtlDelay(20000000);
@@ -36,6 +73,9 @@ int main(void)
         }
         else
         {
+            // Turn on the Output pins
+            PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT | PWM_OUT_6_BIT | PWM_OUT_7_BIT, true);
+
             GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5, 0x14);
 
             SysCtlDelay(20000000);
